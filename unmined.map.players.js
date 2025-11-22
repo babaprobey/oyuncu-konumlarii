1993398ddeb072b221260eb6f91685830f986c10
babaprobey <babaprobabapro00@gmail.com>
2025-11-22 16:25:13 +0300

    Pocisentarci Konum Guncellemesi

--- unmined.map.players.js
+++ unmined.map.players.js
@@ -1,7 +1,6 @@
 var UnminedPlayers = [
-{"name":"pocisentarci","x":-388.7, "z":189.61},
-{"name":"demote","x":-2482.68, "z":-295.41},
-{"name":"Maksur","x":-6.53, "z":828.86},
-{"name":"trabzonspor6115","x":-19.61, "z":828},
-{"name":"UCANDREAM","x":-146.41, "z":806.99},
+{"name":"pocisentarci","x":-388.7, "z":190.74},
+{"name":"demote","x":-2448.58, "z":-290.07},
+{"name":"Maksur","x":-135.93, "z":750.94},
+{"name":"trabzonspor6115","x":-20.21, "z":831.91},
 ];
